b943c86d217564e42ca33db8a579bd4dcc0ebfec
reszkojr <s.reszkojr@gmail.com>
2022-02-01 19:02:25 -0300

    Main class

---
+++ src/main/java/me/reszkojr/CourseApplication.java
@@ -0,0 +1,11 @@
+package me.reszkojr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApplication.class, args);
+    }
+}
